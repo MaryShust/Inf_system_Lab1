ad2f98dac2cd5b9c1f96e686e39bd09eed24021c
MaryShust <maryshust00@gmail.com>
2025-11-24 15:35:54 +0300

    update

--- src/main/resources/static/utils.js
+++ src/main/resources/static/utils.js
@@ -78,7 +78,7 @@ function loadCoordinates(coordinateSelect) {
 function isValidEditText(id, message, className) {
     if (!id) {
         message.textContent = "Введите ID для удаления";
-        message.className = '${className} error';
+        message.className = `${className} error`;
         message.style.display = "block";
         return false;
     }
@@ -86,7 +86,7 @@ function isValidEditText(id, message, className) {
     // Проверка на целое число
     if (!/^\d+$/.test(id)) {
         message.textContent = "ID должен быть целым положительным числом";
-        message.className = '${className} error';
+        message.className = `${className} error`;
         message.style.display = "block";
         return false;
     }
